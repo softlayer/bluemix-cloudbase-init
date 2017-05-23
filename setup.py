7b697b3e562e6fd67aabbc82e464877266d8a902
Mark Price <mprice@softlayer.com>
2017-05-22 20:47:25 -0500

    Added bluemix.conf to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='bluemix',
+setup(name='Bluemix',
       version='0.1',
       description='A bluemix datasource to be used with cloudbase-init',
-      packages=['bluemix'])
+      packages=['bluemix', 'bluemix.conf'])
